b1772fc7b9632a5a2415d898091f4d7ce9aca88c
Unknown <cherif.mohamed86@gmail.com>
2018-08-07 02:26:33 +0100

    1st Assignment
    playground for component

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '[warning]',
+    template: '<h6>Warning works!</h6>',
+    styles: ['h6 {color: red;}'],
+})
+
+export class WarningComponent {
+
+}
