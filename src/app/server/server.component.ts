0f7e624b4da2a0b7f2c40ff34dcb423e7b4cd6f3
Unknown <cherif.mohamed86@gmail.com>
2018-09-09 02:41:25 +0100

    31. Combining all Forms of Databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -21,7 +21,7 @@ export class ServerComponent {
     }
 
     createNewServer() {
-        return this.serverCreationStatus = "Server Created Successfully!";
+        return this.serverCreationStatus = "Server Created Successfully! Name is " + this.serverName;
     }
 
     onServerUpdateName(event: Event) {
